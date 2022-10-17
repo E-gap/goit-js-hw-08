431166dcbffe094dd5f46375fdf7afba5df32a53
E-gap <106596181+E-gap@users.noreply.github.com>
2022-10-18 01:13:12 +0300

    complete homework 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,59 @@
 
+const throttle = require('lodash.throttle');
+
+const email = document.querySelector('[type="email"]');
+const message = document.querySelector('[name="message"]');
+const form = document.querySelector('.feedback-form');
+
+
+
+ 
+const checkLocalStorage = () => {
+const data = localStorage.getItem("feedback-form-state");
+const parsedData = JSON.parse(data); 
+if (parsedData.email) {
+    
+        email.value = parsedData.email;
+};
+    
+if (parsedData.message) {
+        message.value = parsedData.message;
+    } 
+}
+
+
+const object = {}
+
+const fillEmail = function (event) {    
+    
+
+    object.email = event.currentTarget.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(object));
+
+}
+
+const fillMessage = function (event) {  
+    
+    
+    object.message = event.currentTarget.value;
+     localStorage.setItem("feedback-form-state", JSON.stringify(object));
+
+}
+
+const submitForm = function (event) {
+    event.preventDefault();
+    console.log(object);
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+    
+}
+
+
+
+email.addEventListener('input', throttle(fillEmail, 500));
+message.addEventListener('input', throttle(fillMessage, 500));
+form.addEventListener('submit', submitForm);
+document.addEventListener('DOMContentLoaded', checkLocalStorage);
+
+
+
